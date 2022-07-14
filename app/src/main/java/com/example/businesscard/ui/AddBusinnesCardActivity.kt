7b153df3a51bf566c42b635ad315886d4c993805
maria-serafim-dev <maria.serafim.dev@gmail.com>
2022-07-14 11:32:14 -0300

    Verificação de campos vazios e e-mail inválido.

--- app/src/main/java/com/example/businesscard/ui/AddBusinnesCardActivity.kt
+++ app/src/main/java/com/example/businesscard/ui/AddBusinnesCardActivity.kt
@@ -1,6 +1,8 @@
 package com.example.businesscard.ui
 
 import android.os.Bundle
+import android.text.TextUtils
+import android.util.Patterns
 import android.widget.Toast
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
@@ -54,16 +56,65 @@ class AddBusinnesCardActivity : AppCompatActivity() {
     
     private fun insertListener() {
         binding.btnConfirm.setOnClickListener {
-            val businessCard = BusinessCard(
-                name = binding.tlName.editText?.text.toString(),
-                business = binding.tlNameBusiness.editText?.text.toString(),
-                phone = binding.tlPhone.editText?.text.toString(),
-                email = binding.tlEmailAddress.editText?.text.toString(),
-                backgroundColor = colorSelected
-            )
-            mainViewModel.insert(businessCard)
-            Toast.makeText(this, R.string.label_show_success, Toast.LENGTH_LONG).show()
-            finish()
+            if (validarCampos()) {
+                val businessCard = BusinessCard(
+                    name = binding.tlName.editText?.text.toString(),
+                    business = binding.tlNameBusiness.editText?.text.toString(),
+                    phone = binding.tlPhone.editText?.text.toString(),
+                    email = binding.tlEmailAddress.editText?.text.toString(),
+                    backgroundColor = colorSelected
+                )
+                mainViewModel.insert(businessCard)
+                Toast.makeText(this, R.string.label_show_success, Toast.LENGTH_LONG).show()
+                finish()
+            }
         }
+    }
+
+    private fun validarCampos(): Boolean {
+
+        limparErros()
+
+        var retorno = true
+
+        if (TextUtils.isEmpty(binding.teName.text) || binding.teName.text == null) {
+            binding.tlName.error = getString(R.string.error_input_name)
+            binding.teName.requestFocus()
+            retorno = false
+        }
+
+        if (TextUtils.isEmpty(binding.tePhone.text) || binding.tePhone.text == null) {
+            binding.tlPhone.error = getString(R.string.error_input_phone)
+            binding.tePhone.requestFocus()
+            retorno = false
+        }
+
+        if (TextUtils.isEmpty(binding.teEmailAddress.text) || binding.teEmailAddress.text == null) {
+            binding.tlEmailAddress.error = getString(R.string.error_input_email)
+            binding.teEmailAddress.requestFocus()
+            retorno = false
+        } else if (!Patterns.EMAIL_ADDRESS.matcher(binding.teEmailAddress.text.toString())
+                .matches()
+        ) {
+            binding.tlEmailAddress.error = getString(R.string.error_input_email_invalid)
+            binding.teEmailAddress.requestFocus()
+            retorno = false
+        }
+
+        if (TextUtils.isEmpty(binding.teNameBusiness.text) || binding.teNameBusiness.text == null) {
+            binding.tlNameBusiness.error = getString(R.string.error_input_name_business)
+            binding.teNameBusiness.requestFocus()
+            retorno = false
+        }
+
+
+        return retorno
+    }
+
+    private fun limparErros() {
+        binding.tlName.error = null
+        binding.tlPhone.error = null
+        binding.tlEmailAddress.error = null
+        binding.tlNameBusiness.error = null
     }
 }
